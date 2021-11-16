912fb6a34d6eb430d25986275a370e89d62fae9a
Rakkunchoppeneakna <72967225+Rakkunchoppeneakna@users.noreply.github.com>
2021-11-16 09:38:51 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -3,11 +3,16 @@
   <body>
     <table border="1">
       <?php
-      $row = 5;
-      for($i=0;$i<$row;$i++){
-        echo "<tr>";
-        echo "<td>hey!</td>";
-        echo "</tr>";
+      $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>willmeow</td>";
+          echo "<td>is</td>";
+          echo "<td>the</td>";
+          echo "<td>best</td>";
+          echo "<td>cutie</td>";
+          echo "<td>pie</td>";
+          echo "</tr>";
       }
       ?>
     </table>
